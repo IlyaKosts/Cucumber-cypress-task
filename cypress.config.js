642adbe9f3348f9a517987cbaac0d9746df2bd88
IlyaKosts <110360537+IlyaKosts@users.noreply.github.com>
2022-08-15 12:50:32 +0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -18,6 +18,7 @@ module.exports = defineConfig({
     baseUrl: "https://telnyx.com",
     chromeWebSecurity: false,
   },
+  projectId: 'a0000001',
   defaultCommandTimeout: 20000,
   pageLoadTimeout: 40000,
   viewportWidth: 1400,
